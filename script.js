22a02c6ca1c880da328e1d9450537ad4cb6a7da9
SanikaNikam2003 <sanikanikam1392003@gmail.com>
2024-03-26 16:05:19 +0530

    Added my folder to repository

---
+++ script.js
@@ -0,0 +1,13 @@
+function searchJobs() {
+    // Placeholder function for search functionality
+    alert('Search functionality will be implemented in the future.');
+  }
+  
+  function goToJobSeeker() {
+    window.location.href = "index1.html";
+  }
+  
+  function goToEmployer() {
+    window.location.href = 'employer.html';
+  }
+  
